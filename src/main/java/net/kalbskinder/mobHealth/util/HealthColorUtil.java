b636638eeba72e79233f4d77cca4b0d6e69123f3
Kalbskinder <mimosc007@gmail.com>
2026-02-21 17:42:48 +0100

    Add HealthColorUtil and TextUtil for health color management and text formatting

---
+++ src/main/java/net/kalbskinder/mobHealth/util/HealthColorUtil.java
@@ -0,0 +1,16 @@
+package net.kalbskinder.mobHealth.util;
+
+import net.kalbskinder.mobHealth.configuration.Config;
+
+public class HealthColorUtil {
+    public String getSkyblockHealthColor(double health, double maxHealth) {
+        double percentage = (health / maxHealth) * 100.0;
+        if (percentage > 75.0) {
+            return Config.HealthBars.Skyblock.COLOR_HIGH;
+        } else if (percentage >= 25.0) {
+            return Config.HealthBars.Skyblock.COLOR_MID;
+        } else {
+            return Config.HealthBars.Skyblock.COLOR_LOW;
+        }
+    }
+}
---
+++ src/main/java/net/kalbskinder/mobHealth/util/TextUtil.java
@@ -0,0 +1,21 @@
+package net.kalbskinder.mobHealth.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class TextUtil {
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
+
+    private final LegacyComponentSerializer LEGACY =
+            LegacyComponentSerializer.builder()
+                    .character('&')
+                    .hexColors()
+                    .useUnusualXRepeatedCharacterHexFormat()
+                    .build();
+
+    public String legacyToMiniMessage(String input) {
+        Component component = LEGACY.deserialize(input);
+        return miniMessage.serialize(component);
+    }
+}
